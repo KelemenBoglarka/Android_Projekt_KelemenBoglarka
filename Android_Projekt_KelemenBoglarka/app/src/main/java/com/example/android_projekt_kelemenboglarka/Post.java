e1c2a4f7bb645124079abf4083a3b6a7b71404b1
KelemenBoglarka <90687000+KelemenBoglarka@users.noreply.github.com>
2022-01-26 20:47:01 +0200

    Android bemutato

---
+++ Android_Projekt_KelemenBoglarka/app/src/main/java/com/example/android_projekt_kelemenboglarka/Post.java
@@ -0,0 +1,31 @@
+package com.example.android_projekt_kelemenboglarka;
+
+public class Post {
+    private String userId , id , title , body;
+
+    public Post(String userId, String title, String body) {
+        this.userId = userId;
+        this.title = title;
+        this.body = body;
+
+    }
+
+    public String getUserId() {
+
+        return userId;
+    }
+
+    public String getId() {
+
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+
+        return body;
+    }
+}
